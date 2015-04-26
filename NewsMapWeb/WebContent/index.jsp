d513f0ca88b51fdabb9d5604f9b1c9724d54f854
blcarlson <blcarlson@192.168.10.4>
2015-04-26 15:54:34 -0400

    working article popup and display

--- NewsMapWeb/WebContent/index.jsp
+++ NewsMapWeb/WebContent/index.jsp
@@ -5,13 +5,26 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>NewsMap</title>
+<link rel="icon" type="image/png" href="images/favicon.ico">
+<link href="http://fonts.googleapis.com/css?family=Raleway:100,500"
+	rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="css/index.css" />
+<link rel="stylesheet" href="css/ext/jquery.share.css" />
+<link rel="stylesheet" href="css/ext/jBox.css" />
 </head>
 <body>
-	<header> NewsMap </header>
-	<div id="container">
-		<div id="map-canvas"></div>
-	</div>
+	<header>
+		<div class="nav-constrained">
+			<div class="title">
+				<a href="/NewsMapWeb"><span id="thick">NewsMap</span><img
+					class="logo" src="images/newsmaplogo2.png"></a>
+			</div>
+			<ul class="nav nav-right">
+				<li id="listView">List View</li>
+				<li id="about">About</li>
+			</ul>
+		</div>
+	</header>
 	<div id="sidebar">
 		<div class="section">
 			<div class="toggle-triangle carat up-carat"></div>
@@ -30,42 +43,51 @@
 			</div>
 		</div>
 	</div>
-	<section>
-		<div class="articlesHeader">World News | Last 30 Days</div>
-		<article>
-			<h1>
-				<a href="" title=""> SAMPLE Fighting and air strikes across
-					Yemen</a>
-			</h1>
-			<div class="articleSummary">
-				<div class="sourceInformation">World News | Reuters | Sun Mar
-					29, 2015</div>
-				<p>generated summary</p>
-				<p>
-					Individuals: <a href="">Yemeni fighters</a> , President Abd-Rabbu
-					Mansour Hadi, ...
-				</p>
-				<p>Organizations: ... ... ...</p>
-				<p>Related Articles: ... ... ...</p>
-			</div>
-			<div class="articleSentiment">
-				<div>Sentiment: Negative</div>
-				<div>
-					Location: <a href="">Aden</a>
-				</div>
-				<div>
-					Author: <a href="">John Smith</a>
-				</div>
-			</div>
+	<div id="map-container">
+		<div id="map-canvas"></div>
+	</div>
+	<div id="infobar">
+		<h2>
+			NEWS NEAR <span class="country"></span>
+		</h2>
+	</div>
+	<div class="popup-window theme-background is-closed"
+		id="article-window">
+		<img class="button" id="article-close" src="images/close.png">
+		<span class="article-details-small" id="article-category"></span>
+		<span id="share-bar"></span>		
+		<div>
+			<a id="article-title"></a>
+		</div>
+		<span class="article-details-small" id="article-author"></span>
+		<span class="article-sentiment"><a id="article-sentiment" href="#" data-toggle="tooltip" data-placement="top"></a></span>
+		<div id="article">
+			<img id="article-image" />
+			<p id="article-summary"></p>
+		</div>
+		<div class="article-keywords">Keywords: <span id="article-keywords"></span></div>
+		<a id="cluster-link">All related articles</a>
+		<div class="nav-right">
+			<img src="images/back.png" class="button" id="prev-article"> <img
+				src="images/next.png" class="button" id="next-article">
+		</div>
+	</div>
 
-		</article>
-
-	</section>
+	<div class="popup-window theme-background is-closed" id="about-window">
+		<img class="button" id="about-close" src="images/close.png">
+		<h4>NewsMap Version 1.0</h4>
+		<p>Click on a hotspot to view news in that area.</p>
+		<h4>Group Members</h4>
+		<p>Kim Phan (kimphan2)</p>
+		<p>Thomas Hennessey (thennes2)</p>
+		<p>Brandon Carlson (blcrlsn2)</p>
+	</div>
+	<script src="https://apis.google.com/js/platform.js" async defer></script>
 	<script type="text/javascript"
-		src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
-	<script src="js/external/heatmap.min.js"></script>
-	<script src="js/external/gmaps-heatmap.js"></script>
+		src="https://maps.googleapis.com/maps/api/js?libraries=visualization"></script>
 	<script src="js/external/jquery-2.1.3.min.js"></script>
+	<script src="js/external/jquery.share.js"></script>
+	<script src="js/external/jBox.min.js"></script>		
 	<script src="js/index.js"></script>
 </body>
 </html>
--- NewsMapWeb/WebContent/js/index.js
+++ NewsMapWeb/WebContent/js/index.js
@@ -1,93 +1,330 @@
+var heatmap = [];
+var articleIndex = 0;
+var latLng = [];
+var map = [];
+var new_heatmap = [];
 
-$(function(){$("#sidebar .title").parent().on("click",function(a){$("#sidebar .content").is(":hidden")?($("#sidebar .content").slideDown(100),$("#sidebar .toggle-triangle").removeClass("down-carat"),$("#sidebar .toggle-triangle").addClass("up-carat")):($("#sidebar .content").slideUp(100),$("#sidebar .toggle-triangle").removeClass("up-carat"),$("#sidebar .toggle-triangle").addClass("down-carat"))})});function toggleSidebar(){$("#sidebar .title").parent().click()}700>$(window).width()&&$(function(){toggleSidebar()});
+$(document).ready(initialize);
 
+// TODO: review setting a default date or date range here
+function initialize()
+{
+	displayMap();
+	displayArticles();
+}
 
-window.onload = function() {
+// Functionality for the popup box
+$("#next-article").click(function(e) {
+    e.preventDefault();
+    articleIndex += 1;
+    getArticle();
+});
+$("#prev-article").click(function(e) {
+    e.preventDefault();
+    articleIndex -= 1;
+    getArticle();
+});
+$("#article-close").click(function() {
+    articleIndex = 0;
+    closeWindow($("#article-window"));
+    closeinfobar();
+});
 
-        // map center
-        var myLatlng = new google.maps.LatLng(25.6586, -80.3568);
-        // map options,
-        var myOptions = {
-          zoom: 4,
-          center: myLatlng
-        };
-        // standard map
-        map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-        // heatmap layer
-        heatmap = new HeatmapOverlay(map, 
-          {
-            // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-            "radius": 2,
-            "maxOpacity": 1, 
-            // scales the radius based on map zoom
-            "scaleRadius": true, 
-            // if set to false the heatmap uses the global maximum for colorization
-            // if activated: uses the data maximum within the current map boundaries 
-            //   (there will always be a red spot with useLocalExtremas true)
-            "useLocalExtrema": true,
-            // which field name in your data represents the latitude - default "lat"
-            latField: 'lat',
-            // which field name in your data represents the longitude - default "lng"
-            lngField: 'lng',
-            // which field name in your data represents the data value - default "value"
-            valueField: 'count'
-          }
-        );
+// About Box for Group Info
+$("#about-close").click(function() {
+    closeWindow($("#about-window"));
+});
+$("#about").click(function() {
+    displayAboutWindow();
+});
 
-        var testData = {
-          max: 8,
-          data: [{lat: 24.6408, lng:46.7728, count: 3},
-                 {lat: 50.75, lng:-1.55, count: 1},
-                 {lat: 52.6333, lng:1.75, count: 1},
-                 {lat: 48.15, lng:9.4667, count: 1},
-                 {lat: 52.35, lng:4.9167, count: 2},
-                 {lat: 60.8, lng:11.1, count: 1},
-            	{lat: 43.561, lng:-116.214, count: 1},
-            	{lat: 47.5036, lng:-94.685, count: 1},
-            	{lat: 42.1818, lng:-71.1962, count: 1},
-            	{lat: 42.0477, lng:-74.1227, count: 1},
-            	{lat: 40.0326, lng:-75.719, count: 1},
-            	{lat: 40.7128, lng:-73.2962, count: 2},
-            	{lat: 27.9003, lng:-82.3024, count: 1},
-            	{lat: 38.2085, lng:-85.6918, count: 1},
-            	{lat: 46.8159, lng:-100.706, count: 1},
-            	{lat: 30.5449, lng:-90.8083, count: 1},
-            	{lat: 44.735, lng:-89.61, count: 1}
-            	,{lat: 41.4201, lng:-75.6485, count: 2},
-            	{lat: 39.4209, lng:-74.4977, count: 1},
-            	{lat: 39.7437, lng:-104.979, count: 1},
-            	{lat: 39.5593, lng:-105.006, count: 1},
-            	{lat: 45.2673, lng:-93.0196, count: 1},
-            	{lat: 41.1215, lng:-89.4635, count: 1},
-            	{lat: 43.4314, lng:-83.9784, count: 1},
-            	{lat: 43.7279, lng:-86.284, count: 1},
-            	{lat: 40.7168, lng:-73.9861, count: 1},
-            	{lat: 47.7294, lng:-116.757, count: 1},
-            	{lat: 47.7294, lng:-116.757, count: 2},
-            	{lat: 35.5498, lng:-118.917, count: 1},
-            	{lat: 34.1568, lng:-118.523, count: 1},
-            	{lat: 39.501, lng:-87.3919, count: 3},
-            	{lat: 33.5586, lng:-112.095, count: 1},
-            	{lat: 38.757, lng:-77.1487, count: 1},
-            	{lat: 33.223, lng:-117.107, count: 1},{lat: 30.2316, lng:-85.502, count: 1},{lat: 39.1703, lng:-75.5456, count: 8},
-            	{lat: 30.0041, lng:-95.2984, count: 2},{lat: 29.7755, lng:-95.4152, count: 1},{lat: 41.8014, lng:-87.6005, count: 1},
-            	{lat: 37.8754, lng:-121.687, count: 7},{lat: 38.4493, lng:-122.709, count: 1},{lat: 40.5494, lng:-89.6252, count: 1},
-            	{lat: 42.6105, lng:-71.2306, count: 1},{lat: 40.0973, lng:-85.671, count: 1},{lat: 40.3987, lng:-86.8642, count: 1},
-            	{lat: 40.4224, lng:-86.8031, count: 4},{lat: 47.2166, lng:-122.451, count: 1},{lat: 32.2369, lng:-110.956, count: 1},
-            	{lat: 41.3969, lng:-87.3274, count: 2},{lat: 41.7364, lng:-89.7043, count: 2},{lat: 42.3425, lng:-71.0677, count: 1},
-            	{lat: 33.8042, lng:-83.8893, count: 1},{lat: 36.6859, lng:-121.629, count: 2},{lat: 41.0957, lng:-80.5052, count: 1},
-            	{lat: 46.8841, lng:-123.995, count: 1},
-            	{lat: 40.2851, lng:-75.9523, count: 2},
-            	{lat: 42.4235, lng:-85.3992, count: 1},{lat: 39.7437, lng:-104.979, count: 2},{lat: 25.6586, lng:-80.3568, count: 7},
-            	{lat: 33.0975, lng:-80.1753, count: 1},{lat: 25.7615, lng:-80.2939, count: 1},{lat: 26.3739, lng:-80.1468, count: 1},
-            	{lat: 37.6454, lng:-84.8171, count: 1},{lat: 34.2321, lng:-77.8835, count: 1},{lat: 34.6774, lng:-82.928, count: 1},
-            	{lat: 39.9744, lng:-86.0779, count: 1},{lat: 35.6784, lng:-97.4944, count: 2},{lat: 33.5547, lng:-84.1872, count: 1},
-            	{lat: 27.2498, lng:-80.3797, count: 1},{lat: 41.4789, lng:-81.6473, count: 1},{lat: 41.813, lng:-87.7134, count: 1},
-            	{lat: 41.8917, lng:-87.9359, count: 1},{lat: 35.0911, lng:-89.651, count: 1},{lat: 32.6102, lng:-117.03, count: 1},
-            	{lat: 41.758, lng:-72.7444, count: 1},{lat: 39.8062, lng:-86.1407, count: 1},{lat: 41.872, lng:-88.1662, count: 1},
-            	{lat: 34.1404, lng:-81.3369, count: 1},{lat: 46.15, lng:-60.1667, count: 1},{lat: 36.0679, lng:-86.7194, count: 1},
-            	{lat: 43.45, lng:-80.5, count: 1},{lat: 44.3833, lng:-79.7, count: 1},{lat: 45.4167, lng:-75.7, count: 2},{lat: 43.75, lng:-79.2, count: 2},{lat: 45.2667, lng:-66.0667, count: 3},{lat: 42.9833, lng:-81.25, count: 2},{lat: 44.25, lng:-79.4667, count: 3},{lat: 45.2667, lng:-66.0667, count: 2},{lat: 34.3667, lng:-118.478, count: 3},{lat: 42.734, lng:-87.8211, count: 1},{lat: 39.9738, lng:-86.1765, count: 1},{lat: 33.7438, lng:-117.866, count: 1},{lat: 37.5741, lng:-122.321, count: 1},{lat: 42.2843, lng:-85.2293, count: 1},{lat: 34.6574, lng:-92.5295, count: 1},{lat: 41.4881, lng:-87.4424, count: 1},{lat: 25.72, lng:-80.2707, count: 1},{lat: 34.5873, lng:-118.245, count: 1},{lat: 35.8278, lng:-78.6421, count: 1}]
-        };
+function displayAboutWindow() {
+    closeWindow($("#article-window"));
+    openWindow($("#about-window"));
+}
 
-        heatmap.setData(testData);
+function openWindow(e) {
+    if (e.hasClass("is-closed")) {
+        e.slideDown({
+            duration: 100,
+            queue: false
+        });
+        e.animate({
+            opacity: "1.0"
+        }, {
+            duration: 250,
+            queue: false
+        });
+        e.addClass("is-open");
+    }                
+}
+
+function closeWindow(e) {
+    if (e.hasClass("is-open")) {
+        e.slideUp({
+            duration: 100,
+            queue: false
+        });
+        e.animate({
+            opacity: "0.0"
+        }, {
+            duration: 100,
+            queue: false
+        });
+        e.addClass("is-closed");
     }
+}
+
+// Get Article Information
+// TODO: change for how we do articles
+function displayArticles(){
+	// TODO: get articles from the DB
+	var articles = 
+		[{
+		     "pk":634280,
+	         "location":["Moscow", 55.7522200, 37.6155600],
+		     "importance":3445		      
+		   },
+		   {
+			   "pk":634281,
+			   "location":["Paris",48.8534100,2.3488000],
+			   "importance":1000	
+		   }];
+    
+    if (articles.length === 0) {
+        window.alert("Sorry, we don't have any data for this date.");
+        return;
+    }
+    
+    var points = [];
+    for (var i in articles) {
+        latlng = articles[i].location.slice(1, 3);
+        points.push({
+            location: new google.maps.LatLng(latlng[0], latlng[1]),
+            weight: Math.pow(articles[i].importance, 0.4 )
+        });
+    }
+
+    timelapse(new google.maps.MVCArray(points));    
+}
+
+//Uses Google Maps
+function displayMap()
+{
+	var mapOptions = {
+		center: new google.maps.LatLng(30, 0),
+	    zoom: 3,
+	    minZoom: 3,
+	    maxZoom: 4,
+	    // disableDefaultUI: true
+	};
+	
+	// standard map
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+        
+    google.maps.event.addListener(map, "click", function(e) {
+        latLng = e.latLng;
+        $("#infobar-list").empty();
+        articleIndex = 0;
+        getArticle();
+    });
+    
+    setStyle(map);
+}
+
+//Map Style
+//TODO: change color based on sentiment
+function setStyle(e) {
+ var t = [
+{
+ "featureType": "water",
+ "stylers": [
+   { "hue": "#00b2ff" },
+   { "gamma": 0.78 },
+   { "saturation": -84 },
+   { "lightness": -75 }
+ ]
+},{
+ "featureType": "landscape.natural",
+ "stylers": [
+   { "lightness": 20 },
+   { "saturation": -77 },
+   { "gamma": 0.18 }
+ ]
+},
+{
+ "featureType": "administrative",
+ "elementType": "geometry.fill",
+ "stylers": [
+   { "visibility": "off" }
+ ]
+},{
+ "featureType": "water",
+ "elementType": "labels",
+ "stylers": [
+   { "visibility": "off" }
+ ]
+}
+];
+ e.setOptions({
+     styles: t
+ });
+ heatmap = new google.maps.visualization.HeatmapLayer();
+ heatmap.setMap(e);
+ heatmap.set("opacity", 0.80);
+ heatmap.set("radius", 40);
+ var grad = ["rgba(72, 190, 226, 0)",
+             "rgba(72, 190, 226, 1)", 
+             "rgb(100, 203, 230)",
+             "rgb(138, 217, 233)", 
+             "rgb(169, 227, 236)",
+             "rgb(202, 232, 237)",
+             "rgb(212, 231, 234)",
+             "rgba(255, 255, 255, 1)"];
+ heatmap.set("gradient", grad);
+ new_heatmap = new google.maps.visualization.HeatmapLayer();
+ new_heatmap.setMap(e);
+ new_heatmap.set("opacity", 0.80);
+ new_heatmap.set("radius", 40);
+ new_heatmap.set("gradient", grad);
+}
+
+
+function getArticle() {
+	var articles = 
+		[{
+			"pk":634280,
+			"date": "2015-04-25T06:09:45.315Z",
+			"provider": "Reuters",
+			"cluster_url": "http://google.com/HERE", 
+			"summary" : "MOSCOW (Reuters) - Russian authorities said on Sunday they were holding five men over the killing of Kremlin critic Boris Nemtsov, one of whom served in a police unit in the Russian region of Chechnya",
+			"title": "Two charged with Nemtsov killing include Chechen officer: report", 
+			"location": ["Moscow", 55.7522200, 37.6155600], 
+			"article_url": "http://feeds.reuters.com/~r/Reuters/worldNews/~3/zXpgDIDSlYA/story01.htm",
+			"importance": 3445,
+			"image_url": "http://s2.reutersmedia.net/resources/r/?m=02&d=20150308&t=2&i=1030534711&w=580&fh=&fw=&ll=&pl=&r=LYNXMPEB27083",
+			"author": "John Smith",
+			"category": "WorldNews",
+			"sentiment" :{
+				"average" : 1.1234,
+				"longestSentence" : 1,
+				"summary" : 1
+			},
+			"keywords" : [ "worda", "wordb", "wordc"]
+		}];
+		
+		closeWindow($("#about-window"));
+        if (articles.length === 0) {
+            closeWindow($("#article-window"));
+            closeinfobar();
+            return;
+        }               
+         
+        var article = articles[0];
+        if (articleIndex === 0) {
+            $(".country").text(article.location[0].toUpperCase() + " " + article.date.replace(/-/g, "/").toUpperCase());
+        }
+        
+        viewinfobar();        
+        if (articleIndex == 0){
+           	$("#prev-article").hide();
+        }      	           
+        else{
+           	$("#prev-article").show();
+        }	
+        
+        if(articles.length <= 1){
+        	$("#prev-article").hide();
+        	$("#next-article").hide();
+        }      
+        
+        var sentimentText = [ "Very Negative","Negative","Neutral", "Positive", "Very Positive"];
+        
+        $("#article-category").text(article.category + " | " + article.date);
+        $("#article-author").text(article.provider + " | "+ article.author);
+        $("#article-sentiment").text("Sentiment: " + sentimentText[Math.round(article.sentiment.average)]);
+        $("#article-image").attr("src", article.image_url);
+        $("#article-summary").text(article.summary);
+        $("#article-title").text(article.title);
+        $("#article-title").attr("href", article.article_url);
+        $("#article-keywords").text(article.keywords);
+        $("#cluster-link").attr("href", article.cluster_url);
+        $('#share-bar').share({
+            networks: ['facebook','googleplus','twitter','email'],
+            theme: 'square'
+        });                   
+        
+        var jBoxOptions = {
+        	    title: 'Article\'s Sentiment Scores',
+        	    content: "Average Score: " + article.sentiment.average 
+        	    	+ "<br/>Longest Sentence's Score: " + article.sentiment.longestSentence
+        	};
+        $("#article-sentiment").jBox('Tooltip',jBoxOptions);
+        
+        openWindow($("#article-window"));	  
+}
+
+function viewinfobar() {
+    $("#infobar").slideDown({
+        duration: 300,
+        queue: false
+    });
+    $("#infobar").animate({
+        opacity: "0.999"
+    }, {
+        duration: 25,
+        queue: false
+    });
+}
+
+// TODO: review usefulness Interest time lapse
+function timelapse(e) {
+    new_heatmap.set("opacity", 0);
+    new_heatmap.setData(e);
+    heatmap.set("opacity", 0.8);
+    var t = 0;
+    var n = 0.8;
+    var r = setInterval(function() {
+        t += 0.01;
+        n -= 0.01;
+        new_heatmap.set("opacity", t);
+        heatmap.set("opacity", n);
+        if (t >= 0.8) {
+            heatmap.setData(e);
+            setTimeout(function() {
+                heatmap.set("opacity", 0.8);
+                new_heatmap.set("opacity", 0);
+                new_heatmap.setData([]);
+                clearInterval(r);
+            }, 50);
+        }
+    }, 10);
+}
+
+// The info bar shows the "area" you clicked on
+function closeinfobar() {
+    $("#infobar").slideUp({
+        duration: 100,
+        queue: false
+    });
+    $("#infobar").animate({
+        opacity: "0.0"
+    }, {
+        duration: 100,
+        queue: false
+    });
+}
+
+// Handles the show / minimize side bar
+$(function(){
+		$("#sidebar .title").parent().on("click",function(a){
+			$("#sidebar .content").is(":hidden")?($("#sidebar .content").slideDown(100),
+			$("#sidebar .toggle-triangle").removeClass("down-carat"),
+			$("#sidebar .toggle-triangle").addClass("up-carat")):($("#sidebar .content").slideUp(100),
+			$("#sidebar .toggle-triangle").removeClass("up-carat"),$("#sidebar .toggle-triangle").addClass("down-carat"))}
+		)});
+
+function toggleSidebar()
+{
+	$("#sidebar .title").parent().click()	
+}700>$(window).width()&&$(function(){toggleSidebar()});
